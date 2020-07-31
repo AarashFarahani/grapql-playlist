466fb34bf11887aacdcb9e3e529eac6c86104c3d
Arash <arashfarahani@hotmail.com>
2020-07-31 11:26:28 -0700

    graphql in server side has done

--- server/app.js
+++ server/app.js
@@ -1,17 +1,30 @@
 const express = require("express");
-const { graphqlHTTP } = require("express-graphql");
+const {
+    graphqlHTTP
+} = require("express-graphql");
 const schema = require("./schema/schema");
+const dotenv = require("dotenv");
+const mongoose = require('mongoose');
 
 const app = express();
 
+dotenv.config();
+
+// Connect to DB
+mongoose.connect(process.env.DB_CONNECT, {
+        useNewUrlParser: true
+    }, () =>
+    console.log("connected to db!")
+);
+
 app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema,
-    graphiql: true,
-  })
+    "/graphql",
+    graphqlHTTP({
+        schema,
+        graphiql: true,
+    })
 );
 
 app.listen(4000, () => {
-  console.log("now listening for requests on port 4000");
+    console.log("now listening for requests on port 4000");
 });
